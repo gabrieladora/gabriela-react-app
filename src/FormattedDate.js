e6069bc61eb78965e01a4fb2db31ba6826e8ff19
Gabriela <77934204+gabrieladora@users.noreply.github.com>
2021-05-15 10:47:05 +0200

    Added date

---
+++ src/FormattedDate.js
@@ -0,0 +1,16 @@
+export default function FormattedDate(props){
+    //console.log(props.date)
+    
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", ]
+    
+    let day= days[props.date.getDay()] ;
+    let hour=props.date.getHours();
+      if (hour < 10)
+         hour=` 0${props.date.getHours()}`
+    let minutes=props.date.getMinutes();
+       if(minutes < 10)
+        minutes=`0${props.date.getMinutes()}` 
+    
+    
+        return (`${day}, ${hour}:${minutes}`)
+}
