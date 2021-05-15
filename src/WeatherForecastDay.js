4e56e0ae7310145223921d4ed50bcbe6f1be0523
Gabriela <77934204+gabrieladora@users.noreply.github.com>
2021-05-15 11:02:29 +0200

    Work on forecast

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,32 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props){
+    function day(){
+let date= new Date(props.data.dt * 1000)
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let day= date.getDay();
+    
+return days[day];
+}
+    return(
+
+        <div className="WeatherForecastDay">
+         <div className="WeatherForecast-day">{day()}</div> 
+        <WeatherIcon code={props.data.weather[0].icon} size={50} />
+        <div className="WeatherForecast-temperatures">
+          <span className="MaxTemperature"> {Math.round(props.data.temp.max)}℃</span>
+          <span className="MinTemperature">{Math.round(props.data.temp.min)}℃</span>
+          
+        </div>
+ <footer>
+          <em>
+            {" "}
+            <a href="https://github.com/gabrieladora/gabi-react-app" target="_blank" rel="noopener noreferrer">
+              Open-source code
+            </a>{" "}
+            by Gabriela-Dora
+          </em>
+        </footer>   
+      </div> 
+    )}
